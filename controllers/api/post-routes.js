ebc4a0d606899990b6dcc6f9cd10e41a1446dcf7
Nick Lee <raven1300@hotmail.com>
2021-08-29 14:28:00 +1000

    added controller files including API routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,144 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../scripts/auth");
+
+// get all posts
+router.get("/", (req, res) => {
+    Post.findAll({
+        attributes: [
+            "id",
+            "title",
+            "content",
+            "created_at"
+        ],
+        order: [["created_at", "DESC"]],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at"
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get single post
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "title",
+            "content",
+            "created_at"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at"
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with that ID." });
+                return;
+            }
+
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// create a new post
+router.post("/", withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// update post
+router.put("/:id", withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with that ID" });
+                return;
+            }
+
+            res.json(dbPostData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// delete a post
+router.delete("/:id", withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with that ID" });
+                return;
+            }
+
+            res.json(dbPostData);
+        });
+});
+
+module.exports = router;
