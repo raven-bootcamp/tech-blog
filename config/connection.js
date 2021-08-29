540a24f0e9253ffd3542ee0f70de53cd2ab3bc0c
Nick Lee <raven1300@hotmail.com>
2021-08-29 15:35:23 +1000

    updated DB connection environment variable for Heroku

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 let sequelize;
 
 // connect to database
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: "localhost",
